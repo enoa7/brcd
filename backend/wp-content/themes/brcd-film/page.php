1aa6c6c28c021dc55e9f2145fde0beb8448a6482
enoa7 <gamal.ardi@gmail.com>
2016-05-09 22:15:04 +0700

    Frontpage
    add conditional statement on the page.php if its frontpage use template
    'content-frontpage'

--- backend/wp-content/themes/brcd-film/page.php
+++ backend/wp-content/themes/brcd-film/page.php
@@ -20,12 +20,12 @@ get_header(); ?>
 			<?php
 			while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+				if(is_front_page()) {
+					get_template_part( 'template-parts/content', 'frontpage' );
+				} else {
+					get_template_part( 'template-parts/content', 'page' );
+				}
+				
 
 			endwhile; // End of the loop.
 			?>
