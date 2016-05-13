4c0a2077302ad38c97d2dcdca46662006f476474
enoa7 <gamal.ardi@gmail.com>
2016-05-13 23:27:39 +0700

    add custom field for hero-video on frontpage

--- backend/wp-content/themes/brcd-film/template-parts/content-frontpage.php
+++ backend/wp-content/themes/brcd-film/template-parts/content-frontpage.php
@@ -1,5 +1,9 @@
+
+<?php 
+	$video = get_field('hero_video');
+ ?>
 <video id="hero-video" loop autoplay>
-  <source src="<?php echo esc_url( get_stylesheet_directory_uri( '/' ) ); ?>/inc/video/sample-video.mp4" type="video/mp4">
-  <source src="<?php echo esc_url( get_stylesheet_directory_uri( '/' ) ); ?>/inc/video/sample-video.mp4" type="video/ogg">
+  <source src="<?php echo esc_url( get_stylesheet_directory_uri( '/' ) ); ?>/inc/video/<?php echo $video ?>.mp4" type="video/mp4">
+  <source src="<?php echo esc_url( get_stylesheet_directory_uri( '/' ) ); ?>/inc/video/<?php echo $video ?>.mp4" type="video/ogg">
   Oh noes! Your browser does not support the video tag.
 </video>
