d95b9227da597defac013760abcd8a92edd62337
enoa7 <gamal.ardi@gmail.com>
2016-05-13 21:24:06 +0700

    add more info on footer, add motserrat font

--- backend/wp-content/themes/brcd-film/footer.php
+++ backend/wp-content/themes/brcd-film/footer.php
@@ -14,11 +14,16 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
+		<div class="container">
+			<div class="widget-area col-xs-12 col-sm-4" id="more-info">
+		        <?php dynamic_sidebar( 'footer-moreinfo' ); ?>
+		    </div><!-- #more-info .widget-area -->
+		</div>
+		<!-- <div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'brcd-film' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'brcd-film' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'brcd-film' ), 'brcd-film', '<a href="http://kode-in.web.id/" rel="designer">kode-in</a>' ); ?>
-		</div><!-- .site-info -->
+		</div><!-- .site-info --> 
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
