c96747591550b38879679f16d0944bc8420a7436
enoa7 <gamal.ardi@gmail.com>
2016-05-14 00:09:19 +0700

    add what we do section

--- backend/wp-content/themes/brcd-film/template-parts/content-frontpage.php
+++ backend/wp-content/themes/brcd-film/template-parts/content-frontpage.php
@@ -1,6 +1,7 @@
 
 <?php 
 	$video = get_field('hero_video');
+	// $whatwedo = get_field('what_we_do');
  ?>
 
  <div class="hero-content">
@@ -10,3 +11,37 @@
 	  Oh noes! Your browser does not support the video tag.
 	</video>
  </div>
+
+<div class="page-content" id="whatwedo">
+	<h2 class="title text-center">What We Do</h2>
+	<div class="container spacemar">
+		<div class="item col-xs-12 col-sm-4" id="rental">
+			<div class="item-img">
+				<img class="img-responsive center-block" src="<?php echo esc_url( home_url( '/' ) ); ?>/wp-content/uploads/2016/05/rental.png" alt="what we do - rental">
+			</div>
+			<div class="item-title">Rental</div>
+			<div class="item-desc">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+			</div>
+		</div>
+		<div class="item col-xs-12 col-sm-4" id="rental">
+			<div class="item-img">
+				<img class="img-responsive center-block" src="<?php echo esc_url( home_url( '/' ) ); ?>/wp-content/uploads/2016/05/service.png" alt="what we do - service">
+			</div>
+			<div class="item-title">Service</div>
+			<div class="item-desc">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+			</div>
+		</div>
+		<div class="item col-xs-12 col-sm-4" id="rental">
+			<div class="item-img">
+				<img class="img-responsive center-block" src="<?php echo esc_url( home_url( '/' ) ); ?>/wp-content/uploads/2016/05/film-making.png" alt="what we do - film-making">
+			</div>
+			<div class="item-title">Film Making</div>
+			<div class="item-desc">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+			</div>
+		</div>
+	</div>
+</div>
+
