427a55e016bc2bcbbc0ef8b37f1eede471e98ecd
enoa7 <gamal.ardi@gmail.com>
2016-05-21 23:01:15 +0700

    update megamenu for desktop to include logo

--- backend/wp-content/themes/brcd-film/template-parts/megamenu-desktop.php
+++ backend/wp-content/themes/brcd-film/template-parts/megamenu-desktop.php
@@ -8,37 +8,36 @@
 
 <header id="masthead" class="site-header desktop">
 	<div class="container">
-		<div class="site-lang col-sm-9">
-			<span>english</span>
-			<span>|</span>
-			<span>indonesia</span>
+		<div class="site-branding col-sm-4">
+				<?php 
+					$brcd_logo = site_url() . '/wp-content/themes/brcd-film/inc/img/brcd_logo_optimized.svg';
+					echo file_get_contents($brcd_logo);
+				 ?>
 		</div>
-		<div class="site-searchbox col-sm-3">
-			<form class="form-inline">
-			  <div class="form-group">
-			    <div class="input-group">
-			      <input type="text" class="form-control" id="search" placeholder="Search Box">
-			    </div>
-			  </div>
-			</form>
-		</div>
-		<div class="site-wrapper col-xs-12 nopadding">
-			<div class="site-branding col-sm-3">
-				<?php
-				if ( is_front_page() && is_home() ) : ?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php else : ?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php endif; ?>
-			</div><!-- .site-branding -->
-
-			<nav id="site-navigation" class="main-navigation col-sm-9" role="navigation">
+		<div class="col-sm-8 nopadding">
+			<!-- language selection -->
+			<div class="site-lang col-sm-9">
+				<span>english</span>
+				<span>|</span>
+				<span>indonesia</span>
+			</div>
+			<!-- searchbox -->
+			<div class="site-searchbox col-sm-3">
+				<form class="form-inline">
+				  <div class="form-group">
+				    <div class="input-group">
+				      <input type="text" class="form-control" id="search" placeholder="Search Box">
+				    </div>
+				  </div>
+				</form>
+			</div>
+			<div class="site-nav col-sm-12">
+				<nav id="site-navigation" class="main-navigation" role="navigation">
 				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'brcd-film' ); ?></button>
 				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-			</nav><!-- #site-navigation -->
+			</nav> 
+			</div>
 		</div>
-
-
 	</div>
 </header><!-- #masthead -->
 
