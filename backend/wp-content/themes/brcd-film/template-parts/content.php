3084165631dd379e544a8b6453480647c455dcac
enoa7 <gamal.ardi@gmail.com>
2016-05-22 00:38:38 +0700

    remove comments and sidebar

--- backend/wp-content/themes/brcd-film/template-parts/content.php
+++ backend/wp-content/themes/brcd-film/template-parts/content.php
@@ -17,13 +17,7 @@
 			} else {
 				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 			}
-
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php brcd_film_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
+		?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
@@ -40,8 +34,4 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php brcd_film_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
