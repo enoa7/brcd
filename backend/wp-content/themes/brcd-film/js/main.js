5a6f01012dc37f3477f0009e8fa2bb4143b5645e
enoa7 <gamal.ardi@gmail.com>
2016-05-22 12:44:59 +0700

    Item Post
    - add new plugin: Regenerate Thumbnail in case we have new image size
    - remove position absolute from the masthead
    - change gallery image handler to return ID instead of img url so we can
      use custom image size for thumbnail and the main img

--- backend/wp-content/themes/brcd-film/js/main.js
+++ backend/wp-content/themes/brcd-film/js/main.js
@@ -2,10 +2,12 @@ jQuery( document ).ready( function( $ ) {
     // $() will work as an alias for jQuery() inside of this function
     console.log('Causes stain, stay away~');
 
+    // menu toggle for mobile
     $('.site-trigger > .fa').click(function() {
     	$('.main-navigation').toggleClass('toggled');
     });
 
+    // controls for video on homepage
     $('#hero-video').click(function () {
     	var this = $(this);
 		if (this.paused) {
@@ -14,5 +16,16 @@ jQuery( document ).ready( function( $ ) {
 	    else {
 	        this.pause(); 
 	    }
-    })
+    });
+
+    /** controller for the post gallery ------------------ **/
+    var gallery_theatre = $('.category-item #hero-gallery .entry-gallery-theatre > img');
+    var gallery_img = $('.category-item #hero-gallery .item-img');
+    
+    gallery_img.click(function(){
+        var imgUrl = $(this).attr('src');
+        var newUrl = imgUrl.replace('-300x100', '');
+        gallery_theatre.attr('src', newUrl);
+    });
+    /** end ------------------ **/
 } );
--- backend/wp-content/themes/brcd-film/template-parts/content.php
+++ backend/wp-content/themes/brcd-film/template-parts/content.php
@@ -9,29 +9,61 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			} else {
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			}
-		?>
-	</header><!-- .entry-header -->
+<article id="post-<?php the_ID(); ?>" <?php post_class($cat_name)?>>
 
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'brcd-film' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'brcd-film' ),
-				'after'  => '</div>',
-			) );
+	<div class="container">
+		<header class="entry-header">
+			<?php
+				if ( is_single() ) {
+					the_title( '<h1 class="entry-title">', '</h1>' );
+				} else {
+					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+				}
+			?>
+		</header><!-- .entry-header -->
+		<?php 
+			$default = wp_get_attachment_image_src(get_field('img_1'), 'full');
 		?>
-	</div><!-- .entry-content -->
+		<div class="entry-gallery hero" id="hero-gallery">
+			<div class="entry-gallery-theatre">
+				<img class="img-responsive" src="<?php echo $default[0]; ?>">
+			</div>
+			<div class="entry-gallery-thumb">
+				<?php 
+
+					$imgArray = ['img_1', 'img_2'];
+
+					foreach ($imgArray as $img) { 
+						$size = 'gallery_thumb';
+						$attachment_id = get_field($img);
+						$img = wp_get_attachment_image_src($attachment_id, $size);
+				?>
+
+					<div class="item-list">
+						<div class="item-list-thumb">
+							<a class="thumbnail">
+								<img class="img-responsive item-img" src="<?php echo $img[0] ?>" alt="">
+							</a>
+						</div>
+					</div>
+
+				<?php } ?>
+
+			</div>
+		</div>
+		<div class="entry-content">
+			<?php
+				the_content( sprintf(
+					/* translators: %s: Name of current post. */
+					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'brcd-film' ), array( 'span' => array( 'class' => array() ) ) ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				) );
+
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'brcd-film' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
+	</div>
 </article><!-- #post-## -->
