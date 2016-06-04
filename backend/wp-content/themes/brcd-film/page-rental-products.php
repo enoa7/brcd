a3423f5f86ed60e435a4f20d68befad89ef4b525
enoa7 <gamal.ardi@gmail.com>
2016-06-04 13:21:02 +0700

    update/change sass mixins

---
+++ backend/wp-content/themes/brcd-film/page-rental-products.php
@@ -0,0 +1,16 @@
+<?php
+
+/*  -----------------------------------
+ *	Template Page Rental Products
+ *  @package brcdfilm
+ *  ----------------------------------- */ 
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		this is rental product page
+	</main>
+</div>
+
+<?php get_footer(); ?>
