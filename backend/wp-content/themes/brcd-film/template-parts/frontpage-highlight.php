c7d871c483fbc184412c8ec962177b1327bcbea4
enoa7 <gamal.ardi@gmail.com>
2016-06-11 14:40:56 +0700

    update buttons template, add thumbnail validation on frontpage-highlight

--- backend/wp-content/themes/brcd-film/template-parts/frontpage-highlight.php
+++ backend/wp-content/themes/brcd-film/template-parts/frontpage-highlight.php
@@ -9,14 +9,18 @@
 
 <div class="item">
 	<div class="item-img">
-		<?php the_post_thumbnail(); ?>
+		<?php 
+			if(has_post_thumbnail()) {
+				(is_mobile()) ? the_post_thumbnail('mainBanner_xs') : the_post_thumbnail('mainBanner_lg');
+			}
+		?>
 	</div>
 	<div class="item-desc">
 		<div class="container spacepad-15">
 			<div class="item-desc-title"><h2 class="title"><?php the_title(); ?></h2></div>
 			<div class="item-desc-text"><?php echo substr(get_the_excerpt(), 0,250) . ' ...'; ?></div>
 			<div class="item-desc-btn spacepad-15">
-				<a href="<?php echo get_permalink() ?>"><button type="button" class="btn-brcd btn-brcd-secondary">Read More</button></a>
+				<a href="<?php echo get_permalink() ?>"><button type="button" class="btn-brcd btn-brcd-secondary__alt">Read More</button></a>
 			</div>
 		</div>
 	</div>
