2426f69070f3a3894fa0aed8174de7f0e76adeed
enoa7 <gamal.ardi@gmail.com>
2016-06-26 20:10:21 +0700

    update modal

--- backend/wp-content/themes/brcd-film/js/module/modalController.js
+++ backend/wp-content/themes/brcd-film/js/module/modalController.js
@@ -16,13 +16,23 @@ var Modal = {
 
     loadModal: function() {
         var targetModal = Modal.config.btnModal;
-        var html = '<div class="modal-container"></div>';
-        targetModal.parent().append(html);
-        targetModal.one('click', function(event) {
-            event.stopPropagation();
-            var get_url = $(this).attr('href');
-            $('.modal-container').load(get_url).modal('show');
+        // var html = '<div class="modal-container"></div>';
+        // targetModal.parent().append(html);
+        // targetModal.on('click', function(event) {
+        //     event.stopPropagation();
+        //     var get_url = $(this).attr('href');
+        //     $('.modal-container').load(get_url).modal({
+        //     	fadeDuration: 250
+        //     });
+        // });
+
+        $('a.open-modal').click(function(event) {
+            $(this).modal({
+                fadeDuration: 250
+            });
+            return false;
         });
+
     }
 
 }
--- backend/wp-content/themes/brcd-film/template-package.php
+++ backend/wp-content/themes/brcd-film/template-package.php
@@ -46,14 +46,12 @@
 				<div class="section-content entry-content">
 					<?php the_content(); ?>
 					<div class="section-enquire">
-						<!-- <button class="btn-brcd-primary" data-toggle="modal" data-target="#modal-id" href="<?php echo site_url() . '/wp-content/themes/brcd-film/inc/form/enquire-modal.php' ?>" data-enquire="<?php echo get_the_title(); ?>">Enquire Now</button> -->
 						
-						<button class="btn-brcd-primary" data-enquire="<?php echo get_the_title(); ?>">
-							<!-- <a rel="modal:open" href="<?php echo site_url() . '/wp-content/themes/brcd-film/inc/form/enquire-modal.php' ?>">Enquire Now</a> -->
-							<a rel="modal:open" href="#modal-test">Enquire Now</a>
-						</button>
-
-						<div id="modal-test"><p>this is jquery modal plugin <a href="#" rel="modal:close">close modal</a></p></div>
+						<a rel="modal:open" href="<?php  echo site_url() . '/wp-content/themes/brcd-film/inc/form/enquire-modal.php' ?>">
+							<button class="btn-brcd-primary" data-enquire="<?php echo get_the_title(); ?>">
+								Enquire Now
+							</button>
+						</a>
 					</div>
 				</div>
 				
